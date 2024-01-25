a3a78c6f993dc8b3f82b4ec3448e44a35a6cee1f
aazacz <113631218+aazacz@users.noreply.github.com>
2024-01-25 14:16:12 +0530

    updated the readme

--- Frontend/src/Pages/Featurepage1.jsx
+++ Frontend/src/Pages/Featurepage1.jsx
@@ -4,22 +4,19 @@ import axios from 'axios'
 
 
 const Featurepage1 = () => {
-    const [HomeData, setHomeData] = useState([])  
+    const [HomeData, setHomeData] = useState([])
     const handleOnError = (e) => {
-            e.target.src =  "./public/default.jpg";
+        e.target.src = "./public/default.jpg";
     }
 
     useEffect(() => {
         axios.get("http://localhost:3000/ProductDetails").then((response) => {
-            console.log(response.data);
-            setHomeData(response.data)
-            setLoading(false);
+                    setHomeData(response.data)
+                    setLoading(false);
         }).catch((error) => {
-            console.log(error.message);
-            setError(true)
-            seterrorMessage(error.message);
-            setLoading(false);
-
+                    setError(true)
+                    seterrorMessage(error.message);
+                    setLoading(false);
         })
 
 
@@ -28,31 +25,35 @@ const Featurepage1 = () => {
 
 
     return (
-        <div className=' w-full flex items-center justify-center p-32 '>
+        <div className=' w-full flex items-center justify-center p-32 pb-0 '>
             <div className='w-1/2'>
                 <h1 className='text-TextBlue text-4xl font- font-Roboto'> Design & Build Your Own Landing Pages </h1>
                 <p className='pt-2 text-gray-700 '>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
                     mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
-            
-            <div className='pt-5 w-full grid grid-flow-row grid-cols-2  gap-x-3 gap-y-6'>
-               
-                <div>
-                <TitleIcon/>
-                <h1 className='pt-3 text-TextBlue text-xl font- font-Roboto'> Title Goes Here </h1>
-                <p className='text-xs'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. </p>
+
+                <div className='pt-5 w-full grid grid-flow-row grid-cols-2  gap-x-3 gap-y-6'>
+                    {HomeData?.map((values, key) => {
+                        return (
+
+                            <div>
+                                <TitleIcon />
+                                <h1 className='pt-3 text-TextBlue text-xl font- font-Roboto'>  </h1>
+                                <p className='text-xs'>{values.text} </p>
+                            </div>
+
+
+                        )
+                    })}
+
+
+
                 </div>
 
 
-
-
-
-            </div>
-            
-            
             </div>
 
             <div className='w-1/2 flex justify-center'>
-                  <img src="http://localhost:3000/images/mobile-01.png" onError={handleOnError}  alt="No Image Found" className='absolute bottom-0 w-[350px]' />
+                <img src="http://localhost:3000/images/mobile-01.png" onError={handleOnError} alt="No Image Found" className='absolute bottom-0 w-[350px]' />
             </div>
 
         </div>
